f43d3504577ebcd03c00d2afef12c342630daeeb
untodesu <kirill@untode.su>
2025-11-28 13:34:28 +0500

    Fixes for the fix god

--- systemd/prospero-server.cmake
+++ systemd/prospero-server.cmake
@@ -3,9 +3,9 @@
 ## File: generate-service.cmake; Created: Fri Nov 28 2025 11:10:34
 ## Description: Systemd servicefile generation
 
-set(INSTALL_BINDIR "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}")
-set(INSTALL_SYSCONFDIR "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_SYSCONFDIR}")
+set(INSTALL_BINDIR "${CMAKE_INSTALL_PREFIX}/bin")
+set(INSTALL_SYSCONFDIR "${CMAKE_INSTALL_PREFIX}/etc")
 
 configure_file("${CMAKE_CURRENT_LIST_DIR}/prospero-server@.service.in" "${PROJECT_BINARY_DIR}/generated.prospero-server@.service")
 file(INSTALL "${PROJECT_BINARY_DIR}/generated.prospero-server@.service" DESTINATION "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/systemd/system" RENAME "prospero-server@.service")
-file(MAKE_DIRECTORY "$ENV{DESTDIR}${CMAKE_INSTALL_SYSCONFDIR}/prospero")
+file(MAKE_DIRECTORY "$ENV{DESTDIR}/bin/prospero")
